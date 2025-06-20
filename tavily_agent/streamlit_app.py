2e31d06c6f6ad58e557d9e57537780beea40d648
jonatanbuga <yhonabo@ac.sce.ac.il>
2025-06-20 16:07:58 +0300

    adding env file to gitignore

--- tavily_agent/streamlit_app.py
+++ tavily_agent/streamlit_app.py
@@ -14,13 +14,13 @@ if st.button("🚀 Run Agent") and question:
         result: Dict = app.invoke({"question": question})
 
     st.subheader("🧠 Question:")
-    st.write(result.get("question", "N/A"))
+    st.write(result["question"])
 
     st.subheader("🤖 LLM Answer:")
     st.write(result['llm_answer'])
 
     st.subheader("🔗 Retrieved Sources:")
-    sources = result.get("tavily_results", [])
+    sources = result["tavily_results"]
     if sources:
         for i, source in enumerate(sources, 1):
             title = source.get("title", f"Source {i}")
@@ -28,22 +28,3 @@ if st.button("🚀 Run Agent") and question:
             st.markdown(f"{i}. [{title}]({url})")
     else:
         st.write("No sources found.")
-# def run(question: str):
-#     initial_state = {"question": question}
-#     final_result = app.invoke(initial_state)
-#
-#     print("\n🎉 Final Agent Output:\n")
-#
-#     print("🧠 Question:")
-#     print(final_result["question"])
-#
-#     print("\n📚 Retrieved Sources:")
-#     print(final_result["context"])
-#
-#     print("\n🤖 Answer from LLM:")
-#     print(final_result['llm_answer'])
-
-
-#
-# if __name__ == "__main__":
-#     run(input("Teel me someting you want to know?"))
